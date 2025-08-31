33354df987c255c1fef94c7b394921b9fb6f9f6c
mauricio.andre <mauricio.andre@becomex.com.br>
2025-08-31 19:54:23 -0300

    feat: envio de mensgaem de progresso no update external

--- src/Core/ReportCenter.Core/Reports/Handlers/UpdateReportExternalProcessStateHandler.cs
+++ src/Core/ReportCenter.Core/Reports/Handlers/UpdateReportExternalProcessStateHandler.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using ReportCenter.Common.Exceptions;
 using ReportCenter.Common.Localization;
+using ReportCenter.Common.Providers.MessageQueues.Dtos;
+using ReportCenter.Common.Providers.MessageQueues.Enums;
+using ReportCenter.Common.Providers.MessageQueues.Interfaces;
 using ReportCenter.Core.Data;
 using ReportCenter.Core.Reports.Commands;
 using ReportCenter.Core.Reports.Entities;
@@ -14,15 +17,18 @@ public class UpdateReportExternalProcessStateHandler : IRequestHandler<UpdateRep
     private readonly CoreDbContext _coreDbContext;
     private readonly IStringLocalizer<ReportCenterResource> _stringLocalizer;
     private readonly IValidator<UpdateReportExternalProcessStateCommand> _validator;
+    private readonly IMessagePublisher _messagePublisher;
 
     public UpdateReportExternalProcessStateHandler(
         IDbContextFactory<CoreDbContext> dbContextFactory,
         IStringLocalizer<ReportCenterResource> stringLocalizer,
-        IValidator<UpdateReportExternalProcessStateCommand> validator)
+        IValidator<UpdateReportExternalProcessStateCommand> validator,
+        IMessagePublisher messagePublisher)
     {
         _coreDbContext = dbContextFactory.CreateDbContext();
         _stringLocalizer = stringLocalizer;
         _validator = validator;
+        _messagePublisher = messagePublisher;
     }
 
     public async Task Handle(
@@ -31,7 +37,9 @@ public class UpdateReportExternalProcessStateHandler : IRequestHandler<UpdateRep
     {
         await _validator.ValidateAndThrowAsync(request, cancellationToken);
 
-        var entity = await _coreDbContext.Reports.FirstOrDefaultAsync(entity => entity.Id == request.Id);
+        var entity = await _coreDbContext.Reports.FirstOrDefaultAsync(
+            entity => entity.Id == request.Id,
+            cancellationToken);
 
         if (entity == null)
             throw new EntityNotFoundException(_stringLocalizer, nameof(Report), request.Id.ToString());
@@ -44,6 +52,23 @@ public class UpdateReportExternalProcessStateHandler : IRequestHandler<UpdateRep
         entity.ProcessMessage = request.ProcessMessage;
 
         _coreDbContext.Update(entity);
-        await _coreDbContext.SaveChangesAsync();
+        await _coreDbContext.SaveChangesAsync(cancellationToken);
+
+        await _messagePublisher.PublishProgressAsync(
+            new ReportMessageProgressDto(
+                entity.Id,
+                entity.Domain,
+                entity.Application,
+                entity.Version,
+                entity.DocumentName,
+                entity.ReportType,
+                entity.DocumentKey,
+                entity.ProcessState,
+                entity.ProcessTimer,
+                entity.ProcessMessage,
+                entity.ProcessState == ProcessState.Waiting
+            ),
+            cancellationToken: cancellationToken
+        );
     }
 }
