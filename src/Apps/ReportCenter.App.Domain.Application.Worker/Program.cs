312f454cbdd1fc84c8041d2cbb12ab4bbc985c76
mauricio.andre <mauricio.andre@becomex.com.br>
2026-05-02 17:24:01 -0300

    fix: copy MediatR Free version

--- src/Apps/ReportCenter.App.Domain.Application.Worker/Program.cs
+++ src/Apps/ReportCenter.App.Domain.Application.Worker/Program.cs
@@ -1,29 +1,31 @@
+using DocumentFormat.OpenXml.Spreadsheet;
 using FluentValidation;
 using MediatR;
 using ReportCenter.App.Domain.Application.Worker.Reports;
 using ReportCenter.App.GrpcServer.Methods.V1.Examples;
+using ReportCenter.AzureBlobStorages.Extensions;
+using ReportCenter.AzureBlobStorages.Services;
+using ReportCenter.AzureServiceBus.Extensions;
+using ReportCenter.AzureServiceBus.Services;
 using ReportCenter.Common.Diagnostics;
 using ReportCenter.Common.Options;
 using ReportCenter.Common.Providers.MessageQueues.Interfaces;
+using ReportCenter.Common.Providers.OAuth.Interfaces;
 using ReportCenter.Common.Providers.Storage.Interfaces;
 using ReportCenter.Core.Data;
 using ReportCenter.Core.Identity.Interfaces;
 using ReportCenter.Core.Identity.Services;
+using ReportCenter.Core.Reports.Entities;
 using ReportCenter.Core.Reports.Interfaces;
 using ReportCenter.Core.Reports.Services;
 using ReportCenter.Core.Templates.BackgroundServices;
 using ReportCenter.CustomStringLocalizer.Extensions;
+using ReportCenter.LocalStorages.Services;
 using ReportCenter.Mongo.Extensions;
 using ReportCenter.MongoDB.Repositories;
-using ReportCenter.OpenTelemetry.Extensions;
-using ReportCenter.AzureServiceBus.Extensions;
-using ReportCenter.AzureServiceBus.Services;
-using ReportCenter.AzureBlobStorages.Services;
-using ReportCenter.AzureBlobStorages.Extensions;
 using ReportCenter.OAuth.Extensions;
 using ReportCenter.OAuth.Options;
-using ReportCenter.Common.Providers.OAuth.Interfaces;
-using ReportCenter.Core.Reports.Entities;
+using ReportCenter.OpenTelemetry.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -46,8 +48,8 @@ builder.Services
     .AddScoped<ICurrentIdentity, CurrentIdentity>()
     .AddScoped<IReportRepository, ExportRepository>()
     .AddSingleton<IReportServiceFactory, ReportServiceFactory>()
-    // .AddSingleton<IStorageService, LocalStorage>()
-    .AddSingleton<IStorageService, AzureBlobStorage>()
+    .AddSingleton<IStorageService, LocalStorage>()
+    // .AddSingleton<IStorageService, AzureBlobStorage>()
     .AddSingleton<IMessagePublisher, AzureServiceBusPublisher>()
     .AddSingleton<IMessageConsumer, AzureServiceBusConsumer>()
     // .AddScoped<IMessagePublisher, RabbitMQPublisher>()
@@ -77,4 +79,166 @@ builder.Services.AddHostedService<MessageConsumerTemplate>();
 
 var host = builder.Build();
 
-await host.RunAsync();
+var _biggestReportExport = host.Services.GetRequiredService<IBiggestReportExport>();
+
+await using (var stream = _biggestReportExport.OpenWriteStream(
+    Path.Combine("..", "tmp", "file.xlsx"),
+    "example",
+    DateTimeOffset.Now,
+    2))
+{
+
+    stream.SetHeader([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("Texto")
+        },
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("Inteiro")
+        },
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("Decimal")
+        }
+    ]);
+
+    await stream.WriteRowAsync([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("teste")
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(20)
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(1)
+        }
+    ]);
+
+    await stream.WriteRowAsync([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("teste")
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(20)
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(2)
+        }
+    ]);
+
+    stream.EnsureSheet("teste");
+
+    stream.SetHeader([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("Texto")
+        },
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("Inteiro")
+        },
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("Decimal")
+        }
+    ]);
+
+    await stream.WriteRowAsync([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("teste")
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(20)
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(1)
+        }
+    ]);
+
+    await stream.WriteRowAsync([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("teste")
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(20)
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(2)
+        }
+    ]);
+
+    stream.EnsureSheet("example");
+
+    await stream.WriteRowAsync([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("teste")
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(20)
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(3)
+        }
+    ]);
+
+    await stream.WriteRowAsync([
+        new Cell
+        {
+            DataType = CellValues.String,
+            CellValue = new CellValue("teste")
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(20)
+        },
+        new Cell
+        {
+            DataType = CellValues.Number,
+            CellValue = new CellValue(4)
+        }
+    ]);
+
+    stream.EnsureSheet("vazio");
+
+    await stream.SaveAsync();
+}
+
+// await host.RunAsync();
